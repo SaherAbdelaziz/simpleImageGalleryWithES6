645b0fd569779fc32ae4180d47342bea52954432
Saher AbdElaziz <saherabdelaziz@yahoo.com>
2018-08-21 05:09:20 +0200

    add animation

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ function imgClick(e){
     imgs.forEach(img => img.style.opacity = normalOpacity )
 
     current.src = e.target.src;
-
+    current.classList.add('fade-in');
+    setTimeout(() => {
+        current.classList.remove('fade-in');
+    }, 500);
     e.target.style.opacity = opacity ;
     
 }
